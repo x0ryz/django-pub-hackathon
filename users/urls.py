11583328844a66000d1e1334383a4dd8428a7a4a
Hetmanenko Vlad <hetmanenkovlad710@gmail.com>
2026-03-12 09:56:57 +0200

    feat(): sing-up functionality

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DashboardView, LoginView, LogoutView
+from .views import DashboardView, LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     path("", LoginView.as_view()),
+    path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- users/views.py
+++ users/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, TemplateView
+
+from .forms import RegisterForm
 
 
 class LoginView(DjangoLoginView):
@@ -15,6 +18,17 @@ class LoginView(DjangoLoginView):
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard.html"
     login_url = "login"
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "auth/register.html"
+    success_url = reverse_lazy("login")
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("dashboard")
+        return super().dispatch(request, *args, **kwargs)
 
 
 class LogoutView(View):
